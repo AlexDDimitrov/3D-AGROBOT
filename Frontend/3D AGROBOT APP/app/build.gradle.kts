8fe2fd889429b4f911e2c7469d21e6291b7e34f0
Eleonora-Dimitrova <elli.d.dimitrova@gmail.com>
2026-03-26 13:16:34 +0200

    adding a signup, login page and stylings

--- Frontend/3D AGROBOT APP/app/build.gradle.kts
+++ Frontend/3D AGROBOT APP/app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +59,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
---
+++ Frontend/3D AGROBOT APP/app/src/main/java/ui/wizard/SignupData.kt
@@ -0,0 +1,35 @@
+package ui.wizard
+
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+
+class SignupData {
+
+    fun register(
+        first_name: String,
+        last_name: String,
+        email: String,
+        password: String
+    ): String {
+        val url = URL("http://10.196.140.20:5000/auth/register")
+        val conn = url.openConnection() as HttpURLConnection
+
+        conn.requestMethod = "POST"
+        conn.setRequestProperty("Content-Type", "application/json")
+        conn.doOutput = true
+
+        val body = JSONObject().apply {
+            put("first_name", first_name)
+            put("last_name", last_name)
+            put("email", email)
+            put("password", password)
+        }.toString()
+
+        conn.outputStream.write(body.toByteArray())
+        val response = conn.inputStream.bufferedReader().readText()
+        conn.disconnect()
+
+        return response
+    }
+}
