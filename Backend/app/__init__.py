40b5d8158e90513068f2f51eaf61b4f4081f2633
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-03-27 13:40:14 +0200

    Making plant report logic. Ready for testing.

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -11,9 +11,11 @@ def create_app():
     from .routes.auth import auth_bp
     from .routes.garden import garden_bp
     from .routes.garden_request import garden_request_bp
+    from .routes.plant_report import plant_report_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(garden_bp, url_prefix="/garden")
     app.register_blueprint(garden_request_bp, url_prefix="/request")
+    app.register_blueprint(plant_report_bp, url_prefix="/report")
 
     return app
