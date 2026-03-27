aaa90a0687d1dff1b39a026d8b46aef412d61a2b
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-03-27 19:54:03 +0200

    Fixing bugs

--- docking/lib/navigation.py
+++ docking/lib/navigation.py
@@ -38,7 +38,7 @@ def generate_commands(garden: dict) -> list[str]:
     for bed in range(num_beds):
         commands.append(f"M {bed_length}")
         
-        commands.append("C")
+        commands.append("CAPTURE")
         
         if bed < num_beds - 1:
             commands.append(f"M {path_width}")
